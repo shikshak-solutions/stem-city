ad7f5caa28d7f8a80cdf2dd36d326e451cdf06cc
Anamika <cs.0905410020@gmail.com>
2024-07-26 12:08:51 +0530

    Backend code

--- src/pages/ReturnPolicy.js
+++ src/pages/ReturnPolicy.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import NavBar from "../components/navbar/NavBar";
 import Footer from "../footer/Footer";
-import TermsConditions from "../components/policies/TermsConditions";
-import RefundCancellation from "../components/policies/RefundCancellation";
+import Return from "../components/policies/Return";
 const ReturnPolicy = () => {
     return (
         <>
             <NavBar/>
             <div className="container">
-                <ReturnPolicy/>
+                <Return/>
             </div>
             <Footer/>
         </>
