f998f9d9b54dbffbda82dc45a38e55f0127ffd51
Anamika <cs.0905410020@gmail.com>
2024-08-01 11:14:04 +0530

    Added eslint config js

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import pluginReact from 'eslint-plugin-react';
+
+export default [
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    ...pluginJs.configs.recommended,
+    ...pluginReact.configs.flat.recommended,
+    rules: {
+      'no-unused-vars': 'error', // Throw error for unused variables
+      'react/jsx-uses-react': 'error', // Prevent React from being incorrectly marked as unused
+      'react/jsx-uses-vars': 'error', // Prevent variables used in JSX from being incorrectly marked as unused
+    },
+  },
+];
